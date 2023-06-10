2d09e2bf724e274ad18a45d77c035a8ef99f2e44
Veronika <dyoma.veronika@gmail.com>
2023-06-10 21:31:24 +0200

    add hilt for tests

--- app/src/androidTest/java/com/example/drinkrecipes/data/local/DrinksDaoTest.kt
+++ app/src/androidTest/java/com/example/drinkrecipes/data/local/DrinksDaoTest.kt
@@ -1,15 +1,14 @@
 package com.example.drinkrecipes.data.local
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import com.example.drinkrecipes.data.local.db.DrinksDatabase
 import com.example.drinkrecipes.data.local.db.dao.DrinksDao
 import com.example.drinkrecipes.data.local.db.entity.DrinkDetailsEntity
 import com.example.drinkrecipes.data.local.db.entity.DrinkEntity
 import com.google.common.truth.Truth.assertThat
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runBlockingTest
@@ -17,25 +16,28 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
+import javax.inject.Inject
+import javax.inject.Named
 
-@RunWith(AndroidJUnit4::class)
-@SmallTest
 @OptIn(ExperimentalCoroutinesApi::class)
+@SmallTest
+@HiltAndroidTest
 class DrinksDaoTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
 
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    private lateinit var database: DrinksDatabase
+    @Inject
+    @Named("test_db")
+    lateinit var database: DrinksDatabase
     private lateinit var dao: DrinksDao
 
     @Before
     fun setup() {
-        database = Room.inMemoryDatabaseBuilder(
-            ApplicationProvider.getApplicationContext(),
-            DrinksDatabase::class.java
-        ).allowMainThreadQueries().build()
+        hiltRule.inject()
         dao = database.drinksDao()
     }
 
